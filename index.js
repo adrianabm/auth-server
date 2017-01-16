105cde32ab27d3f12866a82b470f6cedbc6c6831
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-16 09:55:11 +0100

    Express route handlers

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // App setup
 // Morgan: logging framework for income requests (mostly used here for debugging)
 // bodyParser: parse income requests into json
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
