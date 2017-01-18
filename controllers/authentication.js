409bb572551b5d28a6c01c84b3811a4a41cbdc70
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-18 22:48:19 +0100

    Sign In users

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,6 +7,12 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret); // sub: subject; iat: issued at time
 }
 
+exports.signin = function(req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need tpo give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
